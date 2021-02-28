31cd0cb4b443aebd25a0d44f98be3af0ba4d7e8a
van1shzz <ppmblast@gmail.com>
2021-02-28 10:46:58 -0500

    added events

--- share-it/src/app/app.component.ts
+++ share-it/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/angular'; // useful for typechecking
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,7 +10,23 @@ import { CalendarOptions } from '@fullcalendar/angular'; // useful for typecheck
 export class AppComponent {
 
   calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth'
+    initialView: 'dayGridMonth',
+
+    //creates events title and across one date
+    dateClick: this.handleDateClick.bind(this), // bind is important!
+    
+    events: [
+      //creates an event range
+      { title: 'event range ', start: '2021-02-27', end: '2021-03-03' },
+      //creates an even over 1 day
+      { title: 'event 2', date: '2021-02-28' }
+    ]
   };
 
+  //alerts the date clicked
+  handleDateClick(arg: { dateStr: string; }) {
+    alert('date click! ' + arg.dateStr)
+  }
+  
+  title: any;
 }
